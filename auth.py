c3d7c72f90ed24cef4bccbb8e8f9c7e6a68444cb
the-farm-guy <thefarmguy011@gmail.com>
2025-05-06 18:28:36 +0000

    initial commit

--- auth.py
+++ auth.py
@@ -72,9 +72,7 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     return encoded_jwt
 
 
-async def get_current_user(
-    token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
-):
+async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -95,9 +93,7 @@ async def get_current_user(
 
 
 @app.post("/token", response_model=Token)
-async def login_for_access_token(
-    form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
-):
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = authenticate_user(db, form_data.username, form_data.password)
     if not user:
         raise HTTPException(
@@ -106,9 +102,7 @@ async def login_for_access_token(
             headers={"WWW-Authenticate": "Bearer"},
         )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
-    )
+    access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
     return {"access_token": access_token, "token_type": "bearer"}
 
 
@@ -132,9 +126,7 @@ async def create_user(user: UserCreate, db: Session = Depends(get_db)):
 
 
 @app.get("/users/all/", response_model=List[UserResponse])
-async def get_users(
-    current_user: User = Depends(get_current_user), db: Session = Depends(get_db)
-):
+async def get_users(current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
     all_users = db.query(User).all()
     return all_users
 
